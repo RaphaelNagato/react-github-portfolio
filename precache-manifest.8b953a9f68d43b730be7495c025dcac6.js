96290c474036c9e31c8bc5bee853897c2488559f
Raphael Okeibunor <raphaelnagato19@gmail.com>
2020-05-15 20:15:38 +0100

    Updates

---
+++ precache-manifest.8b953a9f68d43b730be7495c025dcac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a2a685cd6c3112b0a1f9305321727b",
+    "url": "/react-github-portfolio/index.html"
+  },
+  {
+    "revision": "04069c84b928e979b9cb",
+    "url": "/react-github-portfolio/static/js/2.7e508b67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-github-portfolio/static/js/2.7e508b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a686660fb6f26325221",
+    "url": "/react-github-portfolio/static/js/main.21650ff0.chunk.js"
+  },
+  {
+    "revision": "2d73369cc5b6aeff2e87",
+    "url": "/react-github-portfolio/static/js/runtime-main.dd81a3ef.js"
+  }
+]);
